3591983f50da8d6e09acc8de510dde753ee9a0fb
dmoebrown <53492897+dmoebrown@users.noreply.github.com>
2020-03-24 13:40:37 -0400

    Update fetch_applicants.php

--- gdo_admin_login/htdocs/fetch_applicants.php
+++ gdo_admin_login/htdocs/fetch_applicants.php
@@ -111,39 +111,5 @@ $r = @mysqli_query ($dbc, $q); // Run the query.
 	mysqli_close($dbc); // Close the database connection.
 
 
-// Make the links to other pages, if necessary.
-if ($pages > 1) 
-{
-	
-echo '<br /><p>';
-$current_page = ($start/$display) + 1;
-	
-// If it's not the first page, make a Previous button:
-if ($current_page != 1) 
-{
-	echo '<a href="data.php?s=' . ($start - $display) . '&p=' . $pages . '&sort=' . $sort . '">Previous</a> ';
-}
-	
-// Make all the numbered pages:
-for ($i = 1; $i <= $pages; $i++) 
-{
-	if ($i != $current_page) {
-		echo '<a href="data.php?s=' . (($display * ($i - 1))) . '&p=' . $pages . '&sort=' . $sort . '">' . $i . '</a> ';
-	}
-	else
-	{
-		echo $i . ' ';
-	}
-} // End of FOR loop.
-	
-// If it's not the last page, make a Next button:
-if ($current_page != $pages) 
-{
-	echo '<a href="data.php?s=' . ($start + $display) . '&p=' . $pages . '&sort=' . $sort . '">Next</a>';
-}
-	
-echo '</p>'; // Close the paragraph.
-
-}
 include_once("includes/footer.html");
 ?>
