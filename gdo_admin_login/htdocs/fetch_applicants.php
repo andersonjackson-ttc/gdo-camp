36c961ceb82e36e7227c5142581696d3536469e2
Acearl <42820499+Acearl@users.noreply.github.com>
2020-03-27 15:40:04 -0400

    fetch_applicants
    Improved version over before

--- gdo_admin_login/htdocs/fetch_applicants.php
+++ gdo_admin_login/htdocs/fetch_applicants.php
@@ -1,45 +1,56 @@
 <?php include_once("includes/header.php")?>
-<?php
-	#AUTHOR: Dustin Brown
-
+<?php include_once("includes/frame.html")?>
+<div class="row justify-content-center">
+			<div class=" table-responsive" align="center">
+				<h1>Basic Applicant Information</h1>
+	<form class="form-inline d-flex justify-content-center" action="fetch_applicants.php" method="post">
+	 	<label for="query">Query Type -></label>
+		<select name="query" class="mx-3">
+			<option value="basic">Basic</option>
+			<option value="everything">Everything</option>
+	 		<option value="firstAndLast">First & Last</option>
+	 		<option value="contactInfo">Contact Info</option>
+	 	</select>
+	 	<button type="submit" class="btn btn-primary mb-2">Submit</button>
+ 	</form>
+ <?php
+if($_SERVER['REQUEST_METHOD']=='POST')
+	{
 	 $page_title = 'All Applicants';
 	
-
 	//connect to the database
 	require('../mysqli_connect_applicant_table.php');
-	include_once ('includes/frame.html');
-	
 
 	// Number of records to show per page:
 	$display = 25;
 
 	// Calculate number of pages needed
-if (isset($_GET['p']) && is_numeric($_GET['p'])) 
-{ 
-	$pages = $_GET['p'];
-} 
-else
-{ 
- 	// Count the number of records:
-	$q = "SELECT COUNT(id) FROM applicant";
-	$r = @mysqli_query ($dbc, $q);
-	$row = @mysqli_fetch_array ($r, MYSQLI_NUM);
-	$records = $row[0];
-	// Calculate the number of pages.
-	if ($records > $display) 
+	if (isset($_POST['p']) && is_numeric($_POST['p'])) 
 	{ 
-		$pages = ceil ($records/$display);
+		$pages = $_POST['p'];
 	} 
-	else 
-	{
-		$pages = 1;
-	}
-} // End of IF.
+	else
+	{ 
+	 	// Count the number of records:
+		$q = "SELECT COUNT(id) FROM applicant";
+		$r = @mysqli_query ($dbc, $q);
+		$row = @mysqli_fetch_array ($r, MYSQLI_NUM);
+		$records = $row[0];
+		// Calculate the number of pages.
+		if ($records > $display) 
+		{ 
+			$pages = ceil ($records/$display);
+		} 
+		else 
+		{
+			$pages = 1;
+		}
+	} // End of IF.
 
 // Determine where to start returning records.
-if (isset($_GET['s']) && is_numeric($_GET['s'])) 
+if (isset($_POST['s']) && is_numeric($_POST['s'])) 
 {
-	$start = $_GET['s'];
+	$start = $_POST['s'];
 }
 else 
 {
@@ -48,68 +59,101 @@ else
 
 // Determine the sort
 // Default is by city.
-$sort = (isset($_GET['sort'])) ? $_GET['sort'] : 'fn';
+$sort = (isset($_POST['sort'])) ? $_POST['sort'] : 'fn';
 
 // Determine the sorting order:
 switch ($sort) 
 {
 	case 'fn':
-		$order_by = 'first_name ASC';
+		$order_by = 'FirstName ASC';
 		break;
 	case 'ln':
-		$order_by = 'last_name ASC';
+		$order_by = 'LastName ASC';
 		break;
 	case 'aller':
-		$order_by = 'allergies ASC';
+		$order_by = 'Allergies ASC';
 		break;
 	case 'zip':
-		$order_by = 'zip_code ASC';
+		$order_by = 'ZipCode ASC';
 		break;
 	default:
-		$order_by = 'first_name ASC';
+		$order_by = 'FirstName ASC';
 		$sort = 'fn';
 		break;
 }
-
+	if($_POST['query'] == 'everything')
+	{
+		$q = "SELECT * FROM applicant ORDER BY $order_by LIMIT $start, $display";
+	}
+	elseif($_POST['query'] == 'basic')
+	{
+		$q = "SELECT FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,DateOfBirth,Ethnicity,RisingGradeLevel FROM applicant ORDER BY $order_by LIMIT $start, $display";
+	}
+	elseif($_POST['query'] == 'firstAndLast')
+	{
+		$q = "SELECT FirstName, LastName FROM applicant ORDER BY $order_by LIMIT $start, $display";
+	}
+	elseif($_POST['query'] == 'contactInfo')
+	{
+		$q = "SELECT FirstName, LastName, PhoneNumber FROM applicant ORDER BY $order_by LIMIT $start, $display";
+	//add the querys for parent or emergency contact info
+	}
+	else
+	{
+		$q = "SELECT FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,DateOfBirth,Ethnicity,RisingGradeLevel FROM applicant ORDER BY $order_by LIMIT $start, $display";	
+	}
 // Define the query for all records and fields sorted by the field chosen by the user
-$q = "SELECT * FROM applicant ORDER BY $order_by LIMIT $start, $display";	
+
 //run the query	
 $r = @mysqli_query ($dbc, $q); // Run the query.
-
-	echo' 	<div class="row justify-content-center">
-			<div class="col-md-6 col-md-offset-3" align="center">
-
-	<h1>Basic Applicant Information</h1>
-	<table width="100%">
-		<tr>
-			<th class="text-left"><a href="fetch_applicants.php?sort=fn">First Name</a></th>
-			<th class=""><a href="fetch_applicants.php?sort=ln">Last Name</a></th>
-			<th>DOB</th>
-			<th>Phone</th>
-			<th><a href="fetch_applicants.php?sort=zip">Zip Code</a></th>
-			<th><a href="fetch_applicants.php?sort=aller">Allergies</a></th>
-		</tr>';
-
-	// Fetch and print all the records:
-	while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) 
+ 
+ 	echo'<table width="100%" class="table table-sm table-bordered border-default">
+ 		<tr class="table-dark">';
+ 		$x=0;
+ 		while ($fieldinfo = mysqli_fetch_field($r)) 
+ 		{
+		    echo '<th scope="col">'.$fieldinfo -> name. '</th>';
+		 }
+		echo'</tr>';
+	$x=0;
+	while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) 
 	{
-		echo '<tr><td class="">' . $row['first_name'] . '</td>
-				<td class="">' . $row['last_name'] . '</td>  
-				<td>' . $row['date_of_birth'] . '</td> 
-				<td>' . $row['phone_number'] . '</td> 
-				<td>' . $row['zip_code'] . '</td> 
-				<td>' . $row['allergies'] . '</td>
-			</tr>';
+		$i=0;
+		if(($x % 2) == 0)
+		{
+			$color = "table-warning";
+		}
+		else
+		{
+			$color = "table-info";
+		}
+
+		echo '<tr class='.$color.'>';
+		
+		while($i < mysqli_field_count($dbc))
+		{
+			echo '<td>'.$row[$i].'</td>';
+			$i++;
+		}
+		echo '</tr>';
+		$x++;
 	}
+
 	echo '</table>		
-			</div>
-		</div>
+
 	</div>';
 
 	
 	mysqli_free_result ($r); // Free up the resources.
 	mysqli_close($dbc); // Close the database connection.
 
-
-include_once("includes/footer.html");
+	
+}
+else
+{
+	echo"<p>Press Submit to replace this with Data</p>";
+}
 ?>
+ 	</div>
+</div>
+<?php include_once("includes/footer.html")?>
