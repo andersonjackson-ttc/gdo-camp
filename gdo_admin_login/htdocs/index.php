5da161a347937472b65b50ba4f06772af77a248c
Acearl <42820499+Acearl@users.noreply.github.com>
2020-04-02 15:32:19 -0400

    Add files via upload

--- gdo_admin_login/htdocs/index.php
+++ gdo_admin_login/htdocs/index.php
@@ -4,19 +4,19 @@ include_once("includes/header.php");
 include_once ('includes/frame.html'); 
 ?>
 
-	<h2>Planned features for this particular page</h2>
+	<h2>Administrator landing page</h2>
 	<ul>
-		<li><p>Allow an admin to access every part of the gdo products from web pages</p></li>
 		<li><p>Changing the banner of the homepage and/or template. Ex.</p></li>
-		<input class="btn btn-primary border border-dark" type="submit" value="Submit">Upload new Banner</input>
-		<li><p>Pretty up this page with some bootstrap</p></li>
-		<li><p>Ability to submit a query and return data into a table, sorted if possible</p></li>
-		<li><p>Ability to edit the database records through this webpage</p></li>
+		<form action="uploadBanner.php" method="post" enctype="multipart/form-data">
+			Upload new Banner: PNG ONLY!:
+			<input type="file" name="fileToUpload" id="fileToUpload">
+			<input class="btn btn-primary border border-dark" type="submit" value="Upload Image" name="submit"></input>
+		</form>
 	</ul>
-	<ul class="nav">
-		<a class="btn btn-primary nav-item m-auto border border-dark" href="fetch_applicants.php" role="button">View Records</a>
-		<a class="btn btn-primary nav-item m-auto border border-dark" href="" role="button">Edit Records</a>
-		<a class="btn btn-primary nav-item m-auto border border-dark" href="register.php" role="button">Add new Admin</a>
+	<ul class="nav nav-justified border-dark rounded">
+		<li class="nav-item m-auto"><a class="btn btn-primary border border-dark" href="index.php">Home Page</a></li>
+		<li class="nav-item m-auto"><a class="btn btn-primary border border-dark" href="register.php">Register New Admin</a></li>
+		<li class="nav-item m-auto"><a class="btn btn-primary border border-dark" href="fetch_applicants.php">Basic Query</a></li>
 	</ul>
 	
 <?php include_once("includes/footer.html")?>
