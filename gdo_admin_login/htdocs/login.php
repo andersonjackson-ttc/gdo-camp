f98397d6e68f41e3ccefa1f49d1605425b255539
dmoebrown <53492897+dmoebrown@users.noreply.github.com>
2020-04-14 17:52:58 -0400

    Update login.php

--- gdo_admin_login/htdocs/login.php
+++ gdo_admin_login/htdocs/login.php
@@ -11,7 +11,7 @@
 		$email = $dbc->real_escape_string($_POST['email']);
 		$password = $dbc->real_escape_string($_POST['password']);
 
-		$sql = $dbc->query("SELECT id, name, password FROM users WHERE email='$email'");
+		$sql = $dbc->query("SELECT id, name, password FROM admin WHERE email='$email'");
 		if ($sql->num_rows > 0) {
 		    $data = $sql->fetch_array();
 		    if (password_verify($password, $data['password'])) {
